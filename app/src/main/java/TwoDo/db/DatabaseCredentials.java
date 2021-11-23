04455ee887c7f2ebda16cab8f1e0248815f69d9e
= <=>
2021-11-23 11:07:47 +0530

    Added empty database credentials class

--- app/src/main/java/TwoDo/db/DatabaseCredentials.java
+++
@@ -1,7 +0,0 @@
-package TwoDo.db;
-
-class DatabaseCredentials {
-    public String url = "";
-    public String user = "";
-    public String password = "";
-}
---
+++ app/src/main/java/TwoDo/db/DatabaseCredentialsSample.java
@@ -0,0 +1,9 @@
+package TwoDo.db;
+
+public class DatabaseCredentialsSample {
+    // Rename this to DatabaseCredentials
+    // And enter your user credentials
+    public String url = "";
+    public String user = "";
+    public String password = "";
+}
